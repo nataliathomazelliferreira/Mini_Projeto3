e9dd6c86c0f3044b0f1f76ed1a2f3b5d0dac608b
rmagesto <nataliathomazelliferreira@gmail.com>
2026-05-09 11:18:24 -0300

    implementando API

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 def create_app():
-    app = FastAPI(title="Mini Projeto 3 API")
+    app = FastAPI(title="Mini Projeto 3")
+
+    @app.get("/status")
+    def get_status():
+        return {"status": "online"}
 
     return app
 
